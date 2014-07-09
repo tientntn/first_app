c3ba27ea1d6644cec9a1aad24b7290ed4e6cd6dd
Nguyen tuy tien <nguyentientntn@gmail.com>
2014-07-09 15:04:40 +0700

    testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # comments
+  # comments new
   # GET /users
   # GET /users.json
   def index
